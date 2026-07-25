f07cb3058f6dd07c6dfb7b4ad36bcfec37bd49e2
Samuel Spek <samuel.spek@gmail.com>
2026-07-25 20:48:02 +0200

    Foundation structure changes.

--- app/db.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-
-DATABASE_URL = "sqlite:///./karaoke.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-def init_db():
-    # import models here to register them with SQLModel.metadata
-    from app.models import Song  # local import to avoid circular
-    SQLModel.metadata.create_all(engine)
